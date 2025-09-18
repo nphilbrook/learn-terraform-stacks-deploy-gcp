bf4df13f593a1eb58b0ecaef3aa6c2eca2c6dba4
Robin Norwood <rnorwood@hashicorp.com>
2025-09-18 16:07:59 -0500

    Use resource not data source

--- function/main.tf
+++ function/main.tf
@@ -28,7 +28,7 @@ resource "google_storage_bucket_object" "function_zip" {
   name   = "hello-world.zip"
   bucket = var.storage_bucket_name
   source = local_file.function_zip.filename
-  detect_md5hash = data.local_file.function_zip.content_md5
+  detect_md5hash = local_file.function_zip.content_md5
 
 #  depends_on = [data.local_file.function_zip]
 }
